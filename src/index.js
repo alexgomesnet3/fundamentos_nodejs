455df17697b582dde336eddbf835d2b1064d5164
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-02-13 18:54:02 -0300

    Add Nodemon in App

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (require, response) => {
-    response.json({message:"Hello World Ignite!"})
+    response.json({message:"Hello World Ignite - Fundamentos Node.js"})
 })
 
 app.listen(3333)

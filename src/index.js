d183bce92b30cfabbba4159e302fcb73331ec3fe
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-02-13 23:12:44 -0300

    Add many routes in App

--- src/index.js
+++ src/index.js
@@ -2,8 +2,32 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (require, response) => {
-    response.json({message:"Hello World Ignite - Fundamentos Node.js"})
+/**
+ * GET - Buscar informações
+ * POST - Gravar informações
+ * PUT - Alterar informações
+ * PATCH - Alterar um pedaço da informações
+ * DELETE - Deletar informação
+ */
+
+app.get("/courses", (require, response) => {
+    response.json(["Curso_01","Curso_02","Curso_03"])
+})
+
+app.post("/courses", (require, response) => {
+    response.json(["Curso_01","Curso_02","Curso_03","Curso_04"])
+})
+
+app.post("/courses/:id", (require, response) => {
+    response.json(["Curso_06","Curso_02","Curso_03","Curso_04"])
+})
+
+app.patch("/courses/:id", (require, response) => {
+    response.json(["Curso_06", "Curso_07", "Curso_03", "Curso_04"])
+})
+
+app.delete("/courses/:id", (require, response) => {
+    response.json(["Curso_06", "Curso_07", "Curso_04"])
 })
 
 app.listen(3333)
